5f7b6d1d6dcbb24f4092475b0f50007c39d12777
suxq@yunjiglobal.com <suxq@yunjiglobal.com>
2019-08-14 14:57:13 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # koaren
+
+> 当用 post 方式请求时，我们会遇到一个问题：post 请求通常都会通过表单或 JSON 形式发送，而无论是 Node 还是 Koa，都 没有提供 解析 post 请求参数的功能。
+
+```
+npm i koa-bodyparser -S
+```
+koabody.js
--- koabody.js
+++ koabody.js
@@ -1,20 +1,15 @@
 const Koa = require('koa')
 // 注意 require('koa-router') 返回的是函数:
 const router = require('koa-router')()
+const bodyParser = require('koa-bodyparser')
 const app = new Koa()
 
+app.use(bodyParser());
  // 添加路由
  router.get('/', async (ctx, next) => {
     ctx.response.body = `<h1>index page</h1>`
 })
 
-router.get('/home', async (ctx, next) => {
-    ctx.response.body = '<h1>HOME page</h1>'
-})
-
-router.get('/404', async (ctx, next) => {
-    ctx.response.body = '<h1>404 Not Found</h1>'
-})
 router.get('/user', async(ctx, next)=>{
     ctx.response.body = 
     `
@@ -30,7 +25,8 @@ router.get('/user', async(ctx, next)=>{
   // 增加响应表单请求的路由
   router.post('/user/register',async(ctx, next)=>{
     let {name, password} = ctx.request.body
-    if( name === 'it' && password === 'i' ){
+    console.log(ctx.request.body);
+    if( name === 'it' && password === '1' ){
       ctx.response.body = `Hello， ${name}！`
     }else{
       ctx.response.body = '账号信息错误'
